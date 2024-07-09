03ef2c89e34aeb38f531b886b86d9304800aa66e
Remy Meier <remad@web.de>
2024-07-09 22:35:15 +0200

    master: minimal example of an oauth2 client

---
+++ src/test/java/edu/remad/oauth2_client/Oauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.remad.oauth2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
